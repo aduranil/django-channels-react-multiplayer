f8bf95d48a9829408678957e20456224c0ffe0c2
github username <lina.rudashevski@gmail.com>
2019-04-22 22:33:36 -0400

    django websocket instance

---
+++ frontend/src/Chat.js
@@ -0,0 +1,69 @@
+import React from "react";
+import WebSocketInstance from "./websocket";
+
+class Chat extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.waitForSocketConnection();
+
+    this.waitForSocketConnection(() => {
+      WebSocketInstance.addCallbacks(
+        this.setMessages.bind(this),
+        this.addMessage.bind(this)
+      );
+      WebSocketInstance.fetchMessages(this.props.currentUser);
+    });
+  }
+
+  waitForSocketConnection(callback) {
+    const component = this;
+    setTimeout(function() {
+      if (WebSocketInstance.state() === 1) {
+        console.log("Connection is made");
+        callback();
+        return;
+      } else {
+        console.log("wait for connection...");
+        component.waitForSocketConnection(callback);
+      }
+    }, 100);
+  }
+
+  addMessage = message => {
+    this.setState({ messages: [...this.state.messages, message] });
+  };
+
+  setMessages = messages => {
+    this.setState({ messages: messages.reverse() });
+  };
+
+  renderMessages = messages => {
+    const currentUser = "admin";
+    return messages.map((message, i) => (
+      <li
+        key={message.id}
+        className={message.author === currentUser ? "sent" : "replies"}
+      >
+        <img src="http://emilcarlsson.se/assets/mikeross.png" />
+        <p>
+          {message.content}
+          <br />
+          <small
+            className={message.author === currentUser ? "sent" : "replies"}
+          >
+            {Math.round(
+              (new Date().getTime() - new Date(message.timestamp).getTime()) /
+                60000
+            )}{" "}
+            minutes ago
+          </small>
+        </p>
+      </li>
+    ));
+  };
+
+  render() {
+    return <div> hello </div>;
+  }
+}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDom from "react-dom";
+import Chat from "./Chat";
+import WebSocketInstance from "./websocket";
 
 class App extends React.Component {
+  componentDidMount() {
+    WebSocketInstance.connect();
+  }
   render() {
-    return <div />;
+    return <Chat />;
   }
 }
 
