0496e5c67ea8eda5ff2f92e47d694b10f0062596
github username <lina.rudashevski@gmail.com>
2019-04-22 21:46:21 -0400

    begin frontend

---
+++ frontend/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+class App extends React.Component {
+  render() {
+    return <div />;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
