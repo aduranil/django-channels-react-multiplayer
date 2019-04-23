0e0a1e3639896d56818a37c553811559db0158ca
github username <lina.rudashevski@gmail.com>
2019-04-22 20:18:26 -0400

    remove frontend

---
+++ chat/consumers.py
@@ -0,0 +1,37 @@
+import json
+
+from channels import Group
+from channels.sessions import channel_session
+
+
+# Connected to websocket.connect
+@channel_session
+def ws_connect(message):
+    # Accept connection
+    message.reply_channel.send({"accept": True})
+    # Work out room name from path (ignore slashes)
+    room = message.content['path'].strip("/")
+    # Save room in session and add us to the group
+    message.channel_session['room'] = room
+    Group("chat-%s" % room).add(message.reply_channel)
+
+    Group("chat-%s" % room).send({
+        "text": json.dumps({
+            "type": "WS_HEALTH",
+            "status": "All Good!!"
+        })
+    })
+
+
+# Connected to websocket.receive
+@channel_session
+def ws_message(message):
+    Group("chat-%s" % message.channel_session['room']).send({
+        "text": message['text'],
+    })
+
+
+# Connected to websocket.disconnect
+@channel_session
+def ws_disconnect(message):
+    Group("chat-%s" % message.channel_session['room']).discard(message.reply_channel)
--- selfiesh-frontend/src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-import wsReducer from "./WebSockets";
-
-
-export const reducers = combineReducers({
-  wsReducer,
-});
