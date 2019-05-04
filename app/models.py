ee2ce0e8e48bc5f060c928720e8c7dce6d0149ab
github username <lina.rudashevski@gmail.com>
2019-05-04 17:57:08 -0400

    created game model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Game(models.Model):
+    room_name = models.CharField(max_length=50)
+    users = models.ManyToManyField(User)
