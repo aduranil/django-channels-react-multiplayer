34d55cc7b9cb740263a5b29b59733377b7b6e3b2
github username <lina.rudashevski@gmail.com>
2019-05-10 08:53:30 -0400

    add login method to backend

--- app/views.py
+++ app/views.py
@@ -21,10 +21,22 @@ class GameList(APIView):
 
 
 
-class GetUser(APIView):
+class GetUser(ObtainAuthToken):
     authentication_classes = (TokenAuthentication,)
     def get(self, request, *args, **kwargs):
         return Response({
+            'username': request.user.username,
+            'email': request.user.email,
+        })
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                   context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
             'username': request.user.username,
             'email': request.user.email,
         })
