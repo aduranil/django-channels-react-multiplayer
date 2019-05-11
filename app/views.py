fe0700ae21ef83829fdb50183634db648c7443ea
github username <lina.rudashevski@gmail.com>
2019-05-10 22:49:48 -0400

    commit stuff

--- app/views.py
+++ app/views.py
@@ -6,6 +6,7 @@ from rest_framework.decorators import api_view, authentication_classes, permissi
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import UserSerializer
+from .models import Game
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authtoken.views import ObtainAuthToken
@@ -17,7 +18,13 @@ class GameList(APIView):
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.IsAuthenticated,)
     def post(self, request):
-        return Response({}, status=status.HTTP_201_CREATED)
+        user = request.user
+        game = Game.create(romm_name=request.room_name)
+        game.users.add(user)
+        return Response({
+            'room_name': game.room_name,
+            'player': user.email, 
+        }, status=status.HTTP_201_CREATED)
 
 
 
