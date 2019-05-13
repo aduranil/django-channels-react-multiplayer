94553d4ab55ad0c83fece4657a3a4c470be0f760
github username <lina.rudashevski@gmail.com>
2019-05-12 20:47:07 -0400

    server wont hot reload :(

--- app/views.py
+++ app/views.py
@@ -35,7 +35,7 @@ class LoginUser(ObtainAuthToken):
     def post(self, request, *args, **kwargs):
         serializer = UserSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        token, created = Token.objects.get_or_create(user=serializer)
+        token, created = Token.objects.get_or_create(user=request.data.username)
         return Response({
             'token': token.key,
             'username': serializer.validated_data['username'],
