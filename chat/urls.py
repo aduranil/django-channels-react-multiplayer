ea23eb9669a030923d2023c419795c93fa1cea06
github username <lina.rudashevski@gmail.com>
2019-05-29 22:13:03 -0400

    delete unnecessary files

--- chat/urls.py
+++
@@ -1,9 +0,0 @@
-# chat/urls.py
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('<str:room_name>/', views.room, name='room'),
-]
--- selfies/settings.py
+++ selfies/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
-    'chat',
     'app',
 ]
 
