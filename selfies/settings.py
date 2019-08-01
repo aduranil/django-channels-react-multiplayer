22aca3de3f95b725615f0e5f42d130e242afb2db
Lina Rudashevski <lina.rudashevski@blinkhealth.com>
2019-08-01 15:29:21 -0400

    update

--- selfies/settings.py
+++ selfies/settings.py
@@ -160,7 +160,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_URL = '/selfies-frontend/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'selfies-frontend'),
+]
