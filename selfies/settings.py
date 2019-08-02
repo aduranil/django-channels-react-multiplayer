44ed7650750c11b9634c7e239016e480dbf92a27
github username <lina.rudashevski@gmail.com>
2019-08-01 21:21:11 -0400

    remove connections

--- selfies/settings.py
+++ selfies/settings.py
@@ -110,7 +110,7 @@ WSGI_APPLICATION = 'selfies.wsgi.application'
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
 DATABASES = {
-    'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=500)
+    'default': dj_database_url.config(default=DATABASE_URL)
 }
 
 # DATABASES = {
